442102433bf466f70fdd056b67c2d924ab32375d
SalemCP <a.salem3214@gmail.com>
2022-12-18 21:58:40 +0200

    public 2 ss

--- views/car/Add_Car.php
+++ views/car/Add_Car.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Admin Main</title>
+  <title>Admin Main</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="../css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="../../public/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="../../public/css/animate.css">
     
-    <link rel="stylesheet" href="../css/owl.carousel.min.css">
-    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="../css/magnific-popup.css">
+    <link rel="stylesheet" href="../../public/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../public/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../../public/css/magnific-popup.css">
 
-    <link rel="stylesheet" href="../css/aos.css">
+    <link rel="stylesheet" href="../../public/css/aos.css">
 
-    <link rel="stylesheet" href="../css/ionicons.min.css">
+    <link rel="stylesheet" href="../../public/css/ionicons.min.css">
 
-    <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="../css/jquery.timepicker.css">
+    <link rel="stylesheet" href="../../public/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="../../public/css/jquery.timepicker.css">
 
     
-    <link rel="stylesheet" href="../css/flaticon.css">
-    <link rel="stylesheet" href="../css/icomoon.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../../public/css/flaticon.css">
+    <link rel="stylesheet" href="../../public/css/icomoon.css">
+    <link rel="stylesheet" href="../../public/css/style.css">
   </head>
   <body>
     
@@ -61,7 +61,7 @@
 	  </nav>
     <!-- END nav -->
     
-    <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('../images/test1.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('../../public/images/test1.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
@@ -152,23 +152,23 @@
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-  <script src="../js/jquery.min.js"></script>
-  <script src="../js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="../js/popper.min.js"></script>
-  <script src="../js/bootstrap.min.js"></script>
-  <script src="../js/jquery.easing.1.3.js"></script>
-  <script src="../js/jquery.waypoints.min.js"></script>
-  <script src="../js/jquery.stellar.min.js"></script>
-  <script src="../js/owl.carousel.min.js"></script>
-  <script src="../js/jquery.magnific-popup.min.js"></script>
-  <script src="../js/aos.js"></script>
-  <script src="../js/jquery.animateNumber.min.js"></script>
-  <script src="../js/bootstrap-datepicker.js"></script>
-  <script src="../js/jquery.timepicker.min.js"></script>
-  <script src="../js/scrollax.min.js"></script>
-  <script src="../https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="../js/google-map.js"></script>
-  <script src="../js/main.js"></script>
+  <script src="../../public/js/jquery.min.js"></script>
+  <script src="../../public/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="../../public/js/popper.min.js"></script>
+  <script src="../../public/js/bootstrap.min.js"></script>
+  <script src="../../public/js/jquery.easing.1.3.js"></script>
+  <script src="../../public/js/jquery.waypoints.min.js"></script>
+  <script src="../../public/js/jquery.stellar.min.js"></script>
+  <script src="../../public/js/owl.carousel.min.js"></script>
+  <script src="../../public/js/jquery.magnific-popup.min.js"></script>
+  <script src="../../public/js/aos.js"></script>
+  <script src="../../public/js/jquery.animateNumber.min.js"></script>
+  <script src="../../public/js/bootstrap-datepicker.js"></script>
+  <script src="../../public/js/jquery.timepicker.min.js"></script>
+  <script src="../../public/js/scrollax.min.js"></script>
+  <script src="../../public/https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="../../public/js/google-map.js"></script>
+  <script src="../../public/js/main.js"></script>
     
   </body>
 </html>
