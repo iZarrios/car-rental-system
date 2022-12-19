e7d6cf668d59750e2fc17ec2741e7aba6b5590c1
Zeyad Zakaria <zeyad.zakaria01@gmail.com>
2022-12-19 02:01:58 +0200

    Implementing report2 logic & SQL query (backend)

---
+++ views/reports/report2.php
@@ -0,0 +1,28 @@
+<!-- All reservations of any car within a specified period including all car information. -->
+<?php require_once '../../core/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Report 2</title>
+</head>
+
+<body>
+
+    <!-- Pass data through a form -->
+    <form action="<?= URL . "handlers/reports/r2.php"; ?>" method="POST">
+        <div>
+            <label>date range is from</label>
+            <input type="text" name="lower_date">
+            <label>to</label>
+            <input type="text" name="upper_date">
+        </div>
+        <br>
+        <input type="submit" name="submit" value="submit">
+    </form>
+</body>
+
+</html>
