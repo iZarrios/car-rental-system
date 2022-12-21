a88824c4664dc9044fc9f9918690c28950eac29e
iZarrios <eyad_zarrios2001@yahoo.com>
2022-12-21 21:16:42 +0200

    Add Redirection to Home Page

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php require_once "./core/config.php"; ?>
-<?php require_once PATH . "/core/connection.php" ?>
+<?php require_once './core/config.php'; ?>
+<?php require_once PATH . 'core/connection.php'; ?>
+<?php
 
-
+header("Location: " . URL . "/views/site/index.php");
+?>
