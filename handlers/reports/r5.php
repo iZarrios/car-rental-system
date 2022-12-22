88f88d47cd7a820ef65c2acb8fb9d8fc93a77cad
Zeyad Zakaria <zeyad.zakaria01@gmail.com>
2022-12-22 21:06:39 +0200

    daily payments within specific period

---
+++ handlers/reports/r5.php
@@ -0,0 +1,61 @@
+<!-- Daily payments within specific period. -->
+
+<?php require_once '../../core/config.php'; ?>
+<?php require_once PATH . 'core/connection.php'; ?>
+<?php require_once PATH . 'core/validations.php'; ?>
+<?php
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (isset($_POST['submit']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $errors = [];
+
+    $lower_date = validString($_POST['lower_date']);
+    $upper_date = validString($_POST['upper_date']);
+
+
+    if (empty($errors)) {
+        try {
+            // SELECT reservation_date,SUM(payment) AS "daily_payment" FROM `reservation`
+            // GROUP BY (reservation_date)
+            // HAVING reservation_date BETWEEN '2010-1-1'  AND '2012-1-1';
+            $query = "
+            SELECT reservation_date,SUM(payment) AS 'daily_payment' FROM `reservation`
+            GROUP BY (reservation_date)
+            HAVING reservation_date BETWEEN '$lower_date'  AND '$upper_date';
+            ";
+            $result = mysqli_query($conn, $query);
+
+            $affectedRows = mysqli_affected_rows($conn);
+
+            // close connection
+            mysqli_close($conn);
+
+            // Printing Cars reserved
+            $cars = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+            // dd($cars);
+            if ($affectedRows >= 1) {
+                $_SESSION['report5_result'] = $cars;
+            } else {
+                $errors[] = "Returned 0 results" . "<br>";
+                $_SESSION['errors'] = $errors;
+            }
+            header("Location: " . URL . "views/reports/report5.php");
+            exit;
+        } catch (\Throwable $th) {;
+            $errors[] = $th;
+            $_SESSION['errors'] = $errors;
+            header("Location: " . URL . "views/reports/report5.php");
+        }
+    } else {
+        $_SESSION['errors'] = $errors;
+        header("Location: " . URL . "views/reports/report5.php");
+        exit;
+    }
+}
+?>
