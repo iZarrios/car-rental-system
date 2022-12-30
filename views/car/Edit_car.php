da16dde10f7cce48d356e467f176801f3f6305a6
iZarrios <eyad_zarrios2001@yahoo.com>
2022-12-30 09:14:33 +0200

    Add Float Input Change using Step attribute

--- views/car/Edit_car.php
+++ views/car/Edit_car.php
@@ -31,8 +31,7 @@ if (isset($_GET['plate_id'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="../../public/css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="../../public/css/animate.css">
@@ -59,8 +58,7 @@ if (isset($_GET['plate_id'])) {
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div class="container">
             <a class="navbar-brand" href="../admin/admin.php">ADMIN<span>CONTROLSECTION</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
-                aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="oi oi-menu"></span> Menu
             </button>
 
@@ -68,8 +66,7 @@ if (isset($_GET['plate_id'])) {
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item "><a href="../admin/admin.php" class="nav-link">Home</a></li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink1" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Office
                         </a>
                         <ul class="dropdown-menu ml-auto" aria-labelledby="navbarDropdownMenuLink">
@@ -82,13 +79,11 @@ if (isset($_GET['plate_id'])) {
                         </ul>
                     </li>
                     <li class="nav-item dropdown active">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink1" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Customization Tools
                         </a>
                         <ul class="dropdown-menu ml-auto" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item active " href="<?= URL . "views/car/all_cars.php" ?> "
-                                    class="nav-link ">View cars</a></li>
+                            <li><a class="dropdown-item active " href="<?= URL . "views/car/all_cars.php" ?> " class="nav-link ">View cars</a></li>
                             <li><a class="dropdown-item" href="../car/Add_Car.php" class="nav-link">Add car</a></li>
                             <!-- <li><a class="dropdown-item" href="../car/Edit_car.php" class="nav-link">Customize car</a> -->
                     </li>
@@ -97,8 +92,7 @@ if (isset($_GET['plate_id'])) {
                 </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink2" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Reports
                     </a>
                     <ul class="dropdown-menu ml-auto" aria-labelledby="navbarDropdownMenuLink">
@@ -110,20 +104,17 @@ if (isset($_GET['plate_id'])) {
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink2" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Reservations
                     </a>
                     <ul class="dropdown-menu ml-auto" aria-labelledby="navbarDropdownMenuLink">
                         <li><a class="dropdown-item" href="../reservation/all.php" class="nav-link">View
                                 Reservations</a></li>
-                        <li><a class="dropdown-item" href="../reservation/cancel_reservation.php"
-                                class="nav-link">Delete Reservations</a></li>
+                        <li><a class="dropdown-item" href="../reservation/cancel_reservation.php" class="nav-link">Delete Reservations</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?= URL . "views/site/index.php" ?>" id="navbarDropdownMenuLink2"
-                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link" href="<?= URL . "views/site/index.php" ?>" id="navbarDropdownMenuLink2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         To User Pages
                     </a>
                 </li>
@@ -132,15 +123,12 @@ if (isset($_GET['plate_id'])) {
     </nav>
     <!-- END nav -->
 
-    <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('../../public/images/test1.jpg');"
-        data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('../../public/images/test1.jpg');" data-stellar-background-ratio="0.5">
         <div class="overlay"></div>
         <div class="container">
             <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
                 <div class="col-md-9 ftco-animate pb-5">
-                    <p class="breadcrumbs"><span class="mr-2"><a href="../admin/admin.php">Home <i
-                                    class="ion-ios-arrow-forward"></i></a></span> <span>Edit Car <i
-                                class="ion-ios-arrow-forward"></i></span></p>
+                    <p class="breadcrumbs"><span class="mr-2"><a href="../admin/admin.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Edit Car <i class="ion-ios-arrow-forward"></i></span></p>
                     <h1 class="mb-3 bread">Edit Car</h1>
                 </div>
             </div>
@@ -153,32 +141,26 @@ if (isset($_GET['plate_id'])) {
 
                 <div class="col-md-8 block-9 mb-md-5">
                     <?php require_once PATH . "views/inc/messages.php" ?>
-                    <form class="bg-light p-5 contact-form" method="POST"
-                        action="<?= URL . "handlers/car/update.php" ?>" enctype="multipart/form-data">
+                    <form class="bg-light p-5 contact-form" method="POST" action="<?= URL . "handlers/car/update.php" ?>" enctype="multipart/form-data">
                         <input type="hidden" name="plate_id" id="plate_id" value="<?= $car['plate_id'] ?>" />
 
                         <div class="form-group">
-                            <input type="text" name="brand" id="brand" class="form-control" placeholder="Brand"
-                                value="<?= $car['brand'] ?>" required>
+                            <input type="text" name="brand" id="brand" class="form-control" placeholder="Brand" value="<?= $car['brand'] ?>" required>
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="model" id="model" class="form-control" placeholder="Model"
-                                value="<?= $car['model'] ?>" required>
+                            <input type="text" name="model" id="model" class="form-control" placeholder="Model" value="<?= $car['model'] ?>" required>
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="body" id="body" class="form-control" placeholder="body"
-                                value="<?= $car['body'] ?>" required>
+                            <input type="text" name="body" id="body" class="form-control" placeholder="body" value="<?= $car['body'] ?>" required>
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="color" id="color" class="form-control" placeholder="color"
-                                value="<?= $car['color'] ?>" required>
+                            <input type="text" name="color" id="color" class="form-control" placeholder="color" value="<?= $car['color'] ?>" required>
                         </div>
                         <div class="form-group">
-                            <input type="number" name="year" id="year" class="form-control" placeholder="year"
-                                value="<?= $car['year'] ?>" required>
+                            <input type="number" name="year" id="year" class="form-control" placeholder="year" value="<?= $car['year'] ?>" required>
                         </div>
 
 
@@ -193,21 +175,19 @@ if (isset($_GET['plate_id'])) {
 
 
                         <div class="form-group">
-                            <input type="number" name="price_per_day" id="price_per_day" class="form-control"
-                                placeholder="Price per day" value="<?= $car['price_per_day'] ?>" required>
+                            <input type="number" step="0.01" name="price_per_day" id="price_per_day" class="form-control" placeholder="Price per day" value="<?= $car['price_per_day'] ?>" required>
                         </div>
                         <div class="form-group">
                             <!-- <p><label for="image" style=" cursor: pointer;" class="form-label btn btn-secondary py-0 px-2">Upload Car Image</label></p> -->
                             <!-- <p><input type="file" name="image" id="image" class="form-control" onchange="loadFile(event)" style="display: none;"></p> -->
-                            <p><img id="output" width="200"
-                                    src="../../uploads/images/cars/<?= $car['plate_id'] ?>.jpg" /></p>
+                            <p><img id="output" width="200" src="../../uploads/images/cars/<?= $car['plate_id'] ?>.jpg" /></p>
                         </div>
 
                         <script>
-                        var loadFile = function(event) {
-                            var image = document.getElementById('output');
-                            image.src = URL.createObjectURL(event.target.files[0]);
-                        };
+                            var loadFile = function(event) {
+                                var image = document.getElementById('output');
+                                image.src = URL.createObjectURL(event.target.files[0]);
+                            };
                         </script>
                         <input type="submit" name="submit" value="Edit Car" class="btn btn-primary py-3 px-5">
                     </form>
@@ -224,8 +204,7 @@ if (isset($_GET['plate_id'])) {
             <div class="row mb-5">
                 <div class="col-md">
                     <div class="ftco-footer-widget mb-4">
-                        <h2 class="ftco-heading-2"><a href="../admin/admin.php"
-                                class="logo">Admin<span>ControlSection</span></a></h2>
+                        <h2 class="ftco-heading-2"><a href="../admin/admin.php" class="logo">Admin<span>ControlSection</span></a></h2>
                         <div class="col-md">
                             <div class="ftco-footer-widget mb-4">
                                 <h2 class="ftco-heading-2">Admin Information</h2>
@@ -235,9 +214,7 @@ if (isset($_GET['plate_id'])) {
                                                 Mandara, Alexandria, Egypt</span></li>
                                         <li><a href="#"><span class="icon icon-phone"></span><span class="text">+20 0106
                                                     820 8828</span></a></li>
-                                        <li><a href="https://mail.google.com/"><span
-                                                    class="icon icon-envelope"></span><span
-                                                    class="text">a.salem3214@gmail.com</span></a></li>
+                                        <li><a href="https://mail.google.com/"><span class="icon icon-envelope"></span><span class="text">a.salem3214@gmail.com</span></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -253,8 +230,7 @@ if (isset($_GET['plate_id'])) {
     <!-- loader -->
     <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
             <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
-                stroke="#F96D00" />
+            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" />
         </svg></div>
 
 
@@ -272,8 +248,7 @@ if (isset($_GET['plate_id'])) {
     <script src="../../public/js/bootstrap-datepicker.js"></script>
     <script src="../../public/js/jquery.timepicker.min.js"></script>
     <script src="../../public/js/scrollax.min.js"></script>
-    <script
-        src="../../public/https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
+    <script src="../../public/https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
     </script>
     <script src="../../public/js/google-map.js"></script>
     <script src="../../public/js/main.js"></script>
